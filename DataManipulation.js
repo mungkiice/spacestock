9ed924d002da026171624a4c5d86c51a411db060
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-08-24 10:51:13 +0700

    done task 1.2

--- DataManipulation.js
+++ DataManipulation.js
@@ -12,3 +12,25 @@ function merge(arr1, arr2){
 	});
 	return result;
 }
+
+// 1.2 Object Merge
+invoices = [
+{"to": "John Doe", "amount": 120000},
+{"to": "John Doe", "amount": 80000},
+{"to": "Jane Doe", "amount": 100000}
+]
+
+//write a function to group invoices by the person
+function groupInvoiceByPerson(invoices) {
+	var result = invoices.reduce(function(pass, val){
+		var item = pass.filter(function(o){
+			return o.to == val.to;
+		}).pop() || {to:val.to, amount:0};
+		item.amount += val.amount;
+		pass.push(item);
+		return pass;
+	}, []);
+	return result.filter(function(o, i, arr){
+		return i == arr.indexOf(o);
+	});
+}
