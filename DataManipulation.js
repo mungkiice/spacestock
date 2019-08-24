7c96aa4f9453870ae7afc3b2cb4edb8834064b2d
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-08-24 13:18:08 +0700

    done task 1.3

--- DataManipulation.js
+++ DataManipulation.js
@@ -14,13 +14,12 @@ function merge(arr1, arr2){
 }
 
 // 1.2 Object Merge
-invoices = [
+var invoices = [
 {"to": "John Doe", "amount": 120000},
 {"to": "John Doe", "amount": 80000},
 {"to": "Jane Doe", "amount": 100000}
 ]
 
-//write a function to group invoices by the person
 function groupInvoiceByPerson(invoices) {
 	var result = invoices.reduce(function(pass, val){
 		var item = pass.filter(function(o){
@@ -33,4 +32,21 @@ function groupInvoiceByPerson(invoices) {
 	return result.filter(function(o, i, arr){
 		return i == arr.indexOf(o);
 	});
+}
+
+// 1.3 Finding similar string
+var bannedWords = ["scam", "spam", "dirty"]
+var input1 = "this is a scammer"
+var input2 = "this is a spam message"
+var input3 = "this contains dirty word"
+var input4 = "this is a valid message"
+
+function isBanned(words, bannedWords) {
+    var result = false;
+	bannedWords.forEach(function(bannedWord){
+		if (words.includes(bannedWord)) {
+           result = true;
+		}
+	})
+    return result;
 }
